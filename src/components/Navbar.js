Suryaputhran14/Blockchain-DecentralizedExchange

9. L2 Testnet Deployment

src/components/Navbar.js
@@ -65,7 +65,10 @@ const Navbar = () => {
           <select name="networks" id="networks" value={config[chainId] ? `0x${chainId.toString(16)}` : `0`} onChange={networkHandler}>
             <option value="0" disabled>Select network</option>
             <option value="0x7A69">Localhost</option>
-            <option value="0x5">Goerli</option>
+            <option value="0x5">Goerli (ETH)</option>
+            <option value="0xaa36a7">Sepolia</option>
+            <option value="0x66eed">Goerli (ARB)</option>
+            <option value="0x13881">Mumbai</option>
           </select>
         )}
       </div>

Innovation-Web-3-0-Blockchain/Decentralized-Exchange

src/components/Navbar.js
@@ -65,7 +65,10 @@ const Navbar = () => {
           <select name="networks" id="networks" value={config[chainId] ? `0x${chainId.toString(16)}` : `0`} onChange={networkHandler}>
             <option value="0" disabled>Select network</option>
             <option value="0x7A69">Localhost</option>
-            <option value="0x5">Goerli</option>
+            <option value="0x5">Goerli (ETH)</option>
+            <option value="0xaa36a7">Sepolia</option>
+            <option value="0x66eed">Sepolia (ARB)</option>
+            <option value="0x13881">Mumbai</option>
           </select>
         )}
       </div>